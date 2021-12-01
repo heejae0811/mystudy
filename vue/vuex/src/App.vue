9369bb766a28a7bb7979a2edc0d74346c9a3fa6b
이희재 <heejae0811@gmail.com>
2021-12-01 20:57:05 +0900

    Create vuex store 연습

---
+++ vue/vuex/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+      <button
+        v-on:click="$store.commit('increment')"
+      >증가</button>
+      <button
+        v-on:click="$store.commit('decrement')"
+      >감소</button>
+      <p>{{ count }} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  computed: {
+    count() {
+      return this.$store.state.count; // store count의 상태값
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
